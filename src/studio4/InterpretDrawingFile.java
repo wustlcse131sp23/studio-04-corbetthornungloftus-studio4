3aa2b7720930426482ca67410481b007e5793dd2
jamescorbett <jamescorbett@localhost>
2023-02-23 14:53:45 -0600

    asmnfon

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,21 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		
+		String shape = in.nextLine();
+		int red = in.nextInt();
+		int blue = in.nextInt();
+		int green = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfLength = in.nextDouble();
+
+		
+		//draw
+		StdDraw.setPenColor(Color.green);
+		StdDraw.filledRectangle(x, y, halfWidth, halfLength);
 	}
 }
